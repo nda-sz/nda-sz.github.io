78d6818a0c4911c2257121bc559af7daebddaa4b
zhaophx <110380082+zhaophx@users.noreply.github.com>
2022-12-29 13:16:07 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,60 @@
+function setMenu () {
+    var element = document.getElementById("menu");
+    if (element) {
+        element.innerHTML = "\
+            <li class=\"li-menu\"><a class=\"a-menu\" href=\"index.html\">南大村</a></li>\
+            <li class=\"li-menu\"><a class=\"a-menu\" href=\"notice.html\">公告</a></li>\
+            <li class=\"li-menu\"><a class=\"a-menu\" href=\"picshow.html\">图片展</a></li>\
+            <li class=\"li-right\"><a class=\"a-menu\" href=\"about.html\">关于我们</a></li>"
+    }
+}
+
+function setMain (first, sub) {
+    var element = document.getElementById("main");
+    if (element) {
+        element.innerHTML = "\
+            <p class=\"font-mainTitle\">(first)</p>\
+            <p class=\"font-mainSub\">Nanda Village</p>".replace("(first)", first);
+    } 
+}
+
+function setContent (topic, text, img) {
+    var element = document.getElementById("contents");
+    if (element) {
+        imgHtml = img ? "<img src=\"(img)\" class=\"pic\"><div class=\"tinypic\"\
+            style=\"background:url((img)) no-repeat; background-size:contain;\"></div>" .replace("(img)", img)
+                                                                                        .replace("(img)", img) : "";
+
+        element.innerHTML += "\
+        <div class=\"content\">\
+            (imgHtml)\
+            <p class=\"font-title\" style=\"text-align:center;\"><b><i>(topic)</i></b></p>\
+            <div style=\"margin:100px;\" class=\"font-text\">\
+                <p>(text)</p>\
+            </div>\
+        </div>" .replace("(imgHtml)", imgHtml)
+                .replace("(topic)", topic)
+                .replace("(text)", text);
+    }
+}
+
+function setAd (topic, text, toBtn) {
+    var element = document.getElementById("ads");
+    if (element) {
+
+        btnHtml = toBtn ? "\
+            <a href=\"(toBtn)\">\
+                <button type=\"button\" class=\"learn\">了解更多</button>\
+            </a>"   .replace("(toBtn)", toBtn) : "";
+
+        element.innerHTML += "\
+        <div class=\"ad\">\
+            <p class=\"font-title\" style=\"text-align:center;\"><b><i>(topic)</i></b></p>\
+            <div style=\"margin:50px; text-align:center;\" class=\"font-text\">\
+                <p>(text)</p>(btnHtml)\
+            </div>\
+        </div>" .replace("(topic)", topic)
+                .replace("(text)", text)
+                .replace("(btnHtml)", btnHtml);
+    }
+}
